e5c9b9020e61c9eb8d74b0d6be10ffaa280c900e
Gangsters642 <Ghulamshabir302a@gmail.com>
2026-05-23 06:58:53 +0500

    Day 2 project

---
+++ code4.py
@@ -0,0 +1,17 @@
+name = input("Enter your name =")
+age = input("Enter your age =")
+city = input("Enter your ciry =")
+aim = input("Enter your aim =")
+
+print("----------USER INFO----------")
+
+print("User_Name")
+print(name)
+print("User_age")
+print(age)
+print("User_city")
+print(city)
+print("User_aim")
+print(aim)
+
+print("---------INFO CLOSED----------")
